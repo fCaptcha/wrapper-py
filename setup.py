8b7fb1a3abb138781dc23d695c8fdb7e06c5a3a9
Body-Alhoha <bodyalhoha@gmail.com>
2024-05-15 14:54:34 +0200

    add pypi stuff

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fcaptcha',
+    version='0.0.3',
+    author='Body Alhoha',
+    author_email='bodyalhoha@gmail.com',
+    description='Official fCaptcha Wrapper',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    long_description="# Python Wrapper for fCaptcha<br>Official fCaptcha Python wrapper",
+    long_description_content_type="text/markdown",
+    url="https://github.com/fCaptcha/wrapper-py",
+    license="MIT",
+
+)
